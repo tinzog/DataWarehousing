dfc7d94424a90173f3b0f37886fd7516cf96479d
Christine Zoglmeier <christine.zoglmeier@stud.th-rosenheim.de>
2023-11-21 19:42:56 +0100

    [BUGFIX] Fix missing go to errors

--- setup.bat
+++ setup.bat
@@ -4,33 +4,59 @@ SET SA_PASSWORD=Password+123
 SET SSQL_PORT=2019
 SET CONTAINER_NAME=SQL19
 
-REM Main entry point
 call :init-all
 goto end
 
-REM Function to pull Docker image
 :pull-image
 echo Pulling Docker image...
 docker pull mcr.microsoft.com/mssql/server:2019-latest
-goto:eof
+goto eof
 
-REM Function to run Docker container
 :run-container
 echo Running Docker container...
 docker rm -f %CONTAINER_NAME% 2> NUL
 docker run --name %CONTAINER_NAME% -p %SSQL_PORT%:1433 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=%SA_PASSWORD%" -d mcr.microsoft.com/mssql/server:2019-latest
-goto:eof
+goto eof
 
-REM Function to setup backup
 :setup-backup
 echo Setting up backup...
 docker exec -it SQL19 mkdir /var/opt/mssql/backup 2> NUL
 docker cp databases/Quelle_csv_load.bak SQL19:/var/opt/mssql/backup
 docker cp databases/Quelle_OLTP_System.bak SQL19:/var/opt/mssql/backup
-goto:eof
+goto eof
 
-REM Function to restore database
 :restore-db
 echo Restoring database...
 docker exec -it SQL19 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "%SA_PASSWORD%" -Q "RESTORE DATABASE Quelle_csv FROM DISK = '/var/opt/mssql/backup/Quelle_csv_load.bak' WITH MOVE 'Quelle_csv' TO '/var/opt/mssql/data/Quelle_csv', MOVE 'Quelle_csv_log' TO '/var/opt/mssql/data/Quelle_csv_log'"
-docker exec -it SQL19 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "%SA_PASSWORD%" -Q "RESTORE DATABASE Quelle_OLTP FROM DISK = '/var/opt/mssql/backup/Quelle_OLTP_System.bak' WITH MOVE 'Quelle_OLTP_System' TO '/
+docker exec -it SQL19 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "%SA_PASSWORD%" -Q "RESTORE DATABASE Quelle_OLTP FROM DISK = '/var/opt/mssql/backup/Quelle_OLTP_System.bak' WITH MOVE 'Quelle_OLTP_System' TO '/var/opt/mssql/data/Quelle_OLTP_System', MOVE 'Quelle_OLTP_System_log' TO '/var/opt/mssql/data/Quelle_OLTP_System_log'"
+goto eof
+
+:info
+echo Displaying connection information...
+echo You can connect to the SQL Server using the following information:
+echo Username: SA
+echo Password: %SA_PASSWORD%
+echo Port: %SSQL_PORT%
+goto eof
+
+:stop
+echo Stopping container...
+docker stop %CONTAINER_NAME%
+goto eof
+
+:start
+echo Starting container...
+docker start %CONTAINER_NAME%
+goto eof
+
+:init-all
+echo Initializing all...
+call :pull-image
+call :run-container
+call :setup-backup
+call :restore-db
+call :info
+goto eof
+
+:end
+echo Script execution completed.
